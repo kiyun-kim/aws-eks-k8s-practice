5eef4b9b56b581db203c2f5095604968fadc9c8c
kiyun-kim <kiyn2403@gmail.com>
2026-01-03 03:13:57 +0900

    fix: eks_name 변경

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -3,6 +3,6 @@ locals {
   region      = "ap-northeast-2"
   zone1       = "ap-northeast-2a"
   zone2       = "ap-northeast-2b"
-  eks_name    = "curry"
+  eks_name    = "demo"
   eks_version = "1.33"
 }
